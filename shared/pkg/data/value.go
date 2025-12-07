45f94ca0dd7a1e7953ac93d65124ca65b29be060
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-07 14:57:45 -0500

    Value can now push bool

--- shared/pkg/data/value.go
+++ shared/pkg/data/value.go
@@ -20,6 +20,16 @@ func NewValue(v any) (Value, error) {
 			Type: types.LoInt,
 			Data: data,
 		}, nil
+	case bool:
+		data := make([]byte, 1)
+		if x {
+			data[0] = 1
+		}
+
+		return Value{
+			Type: types.LoBool,
+			Data: data,
+		}, nil
 	}
 
 	return Value{}, errors.New("invalid value type")
@@ -27,4 +37,8 @@ func NewValue(v any) (Value, error) {
 
 func (v *Value) GetInt() int64 {
 	return int64(v.Data[0])
+}
+
+func (v *Value) GetBool() bool {
+	return v.Data[0] == 1
 }
