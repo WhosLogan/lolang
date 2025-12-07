52a150c930e2cdfef0c3f3e1f5e08272ac1b6906
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-07 14:41:23 -0500

    Add vm ctx to function ctx

--- runtime/internal/vm/function/ctx.go
+++ runtime/internal/vm/function/ctx.go
@@ -1,11 +1,13 @@
 package function
 
 import (
+	"runtime/internal/vm"
 	"shared/pkg/data"
 	"shared/pkg/function"
 )
 
 type Ctx struct {
+	Vm        *vm.VCtx
 	Function  *function.Function
 	Stack     data.Stack[data.Value]
 	Locals    []data.Value
